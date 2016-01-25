f49bec4517c7ca57e6e0fe1dc3296117327265bd
jmartin <jmartin@zikzakmedia.com>
2016-01-25 15:37:39 +0100

    Remove auto uninstallation of module

--- configuration.py
+++ configuration.py
@@ -2,10 +2,7 @@
 # The COPYRIGHT file at the top level of this repository contains
 # the full copyright notices and license terms.
 from trytond.model import fields
-from trytond.pool import Pool, PoolMeta
-from trytond.transaction import Transaction
-
-from sql import Literal
+from trytond.pool import PoolMeta
 
 
 __all__ = ['Configuration']
@@ -16,19 +13,3 @@ class Configuration:
     __name__ = 'product.configuration'
     esale_attribute_group = fields.Property(fields.Many2One('esale.attribute.group',
             'eSale Attribute Group'))
-
-    @classmethod
-    def __register__(cls, module_name):
-        pool = Pool()
-        Module = pool.get('ir.module')
-
-        cursor = Transaction().cursor
-        module = Module.__table__()
-
-        cursor.execute(*module.update(
-                columns=[module.state],
-                values=[Literal('to remove')],
-                where=module.name == Literal('product_configuration')
-                ))
-
-        super(Configuration, cls).__register__(module_name)
